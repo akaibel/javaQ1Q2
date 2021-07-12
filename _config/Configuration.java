c0f5983773bdabd093c27c2e4760f632cad6cf2c
akaib <akaib@DESKTOP-O1NN7LL.fritz.box>
2021-07-12 22:27:12 +0200

    Dokumentation von Configuration.java

--- _config/Configuration.java
+++ _config/Configuration.java
@@ -2,8 +2,23 @@ package _config;
 
 public class Configuration {
 
+	/**
+	 * Wartezeit, damit lineare Datenstrukturen 
+	 * (StackWithViewer, QueueWithViewer, ListWithViewer)
+	 * langsam ablaufen 
+	 */
 	public static final int WARTEZEIT_LINEAR = 100;
+	/**
+	 * Wartezeit, damit Binaerbaeume 
+	 * (TreeViewer)
+	 * langsam ablaufen 
+	 */
 	public static final int WARTEZEIT_BAEUME = 500;
+	/**
+	 * Wartezeit, damit Graphen 
+	 * (GraphWithViewer) 
+	 * langsam ablaufen.
+	 */
 	public static final int WARTEZEIT_GRAPH = 50;
 
 }
