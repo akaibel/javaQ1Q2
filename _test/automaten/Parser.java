62beeac651706414fb2d12df2d511bfca1c5a059
akaibel <a.kaibel@googlemail.com>
2023-11-01 11:01:48 +0100

    new class Parser for universal usage.

---
+++ _test/automaten/Parser.java
@@ -0,0 +1,23 @@
+package _test.automaten;
+
+import gui.GUI;
+
+public class Parser {
+	private char[] alphabet = {'a','b','c','x'};
+	
+	public boolean imAlphabet(char pZeichen){
+		//TODO
+		return false;
+	}
+	
+	public boolean teste(String pEingabe){
+		int zustand = 0;
+		//TODO
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		new GUI(new Parser());
+	}
+	
+}
--- _test/automaten/spionage/KGBAutomat.java
+++ _test/automaten/spionage/KGBAutomat.java
@@ -3,7 +3,7 @@ package _test.automaten.spionage;
 import gui.GUI;
 
 public class KGBAutomat {
-	public int zustand;
+	
 	private char[] alphabet = {'0','1','2','3','4','5','6','7','8','9'};
 	
 	public boolean imAlphabet(char pZeichen){
@@ -12,7 +12,7 @@ public class KGBAutomat {
 	}
 	
 	public boolean teste(String pEingabe){
-		zustand = 0;
+		int zustand = 0;
 		//TODO
 		return false;
 	}
