4bda1b049db4bf8dac8cf9072336c8e3968a9979
Andreas Kaibel <AndKai@sibi-honnef.de>
2023-11-22 15:27:28 +0100

    Kontrolldaten

--- _test/array/WetterDaten2018.java
+++ _test/array/WetterDaten2018.java
@@ -3,6 +3,13 @@ package _test.array;
 import gui.GUI;
 
 public class WetterDaten2018 {
+
+	// Kontrollwerte: 
+	// gesamter Niederschlag im Jahr: 2352
+	// gesamter Niederschlag fuer pMonat = 3: 239
+	// hoechster Niederschlag im Jahr: 32
+	
+
 	private int[][] niederschlaege = {
 			{},
 			{ 3,12, 7,19, 6, 0, 0,17, 5, 2, 0, 1, 0, 3, 8,11,12,10,21, 9, 3, 0, 0, 0, 2, 5, 8,11,11,12},	
