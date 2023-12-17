fa0f6f029030e93d40bba003ce5a277c2f13c0ec
akaibel <a.kaibel@googlemail.com>
2023-12-17 09:16:02 +0100

    imprived MailClient.java

--- netz/MailClientTest.java
+++ netz/MailClientTest.java
@@ -7,19 +7,37 @@ public class MailClientTest {
 	private String passwort = "geheim";
 	private String server = "localhost";
 	private int portPop3 = 110;
+	private int portSMTP = 25;
+	private String mailadresse = "anna@kolleg.de";
 	
 	public String getMail(int nr) {
+		String zumServer, antwort, mailtext;
 		System.out.println("Connection("+server+","+portPop3);
+
 		Connection co = new Connection(server,portPop3);
-		String antwort1 = co.receive();
-		System.out.println(antwort1);
-		if(!antwort1.startsWith("+OK")) {
+		antwort = co.receive();
+		System.out.println(antwort);
+		if(!antwort.startsWith("+OK")) {
 			co.close();
 			return "Verbindung fehlgeschlagen";
 		}
+
+		zumServer = "USER "+user;
+		System.out.println(zumServer);
+		co.send(zumServer);
+		antwort = co.receive();
+		System.out.println(antwort);
+		if(!antwort.startsWith("+OK")) {
+			co.close();
+			return "Verbindung fehlgeschlagen";
+		}
+		
 		//TODO
-		return antwort1;
+		mailtext = "TODO";
+		return mailtext;
 	}
+	
+	
 	
 	
 	public static void main(String[] args) {
