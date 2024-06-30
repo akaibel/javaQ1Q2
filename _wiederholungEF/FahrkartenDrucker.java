f5a5391654cb22feab3835702bd3ad0f9cfc8557
akaibel <a.kaibel@googlemail.com>
2024-06-30 18:47:33 +0200

    Anzeige verbessert.

--- _wiederholungEF/FahrkartenDrucker.java
+++ _wiederholungEF/FahrkartenDrucker.java
@@ -13,7 +13,7 @@ public class FahrkartenDrucker {
 	private GUI druckerGUI;
 	
 	public FahrkartenDrucker(){
-		vorhandeneBlaetter = 2;
+		vorhandeneBlaetter = 3;
 		druckerGUI = new GUI(this);
 	}
 	
@@ -34,13 +34,18 @@ public class FahrkartenDrucker {
 		fahrkartenText += (ueberschrift+"\n");
 		fahrkartenText += (sternchen(sternchenZahl));
 		fahrkartenText += " "+pFahrkarte+" ";
-		fahrkartenText += (sternchen(sternchenZahl) + "\n");
+		if(pFahrkarte.length() % 2 == 0) {
+			fahrkartenText += (sternchen(sternchenZahl+1) + "\n");			
+		}
+		else {
+			fahrkartenText += (sternchen(sternchenZahl) + "\n");						
+		}
 		fahrkartenText += (sternchen(ueberschrift.length())+"\n");
 		fahrkartenText += ("\n");
 		fahrkartenText += ("\n");
 		//System.out.println(fahrkartenText);
 		JTextArea textArea = new JTextArea();
-        Font courierNewFont = new Font("Courier New", Font.PLAIN, Configuration.FONT_SIZE);
+        Font courierNewFont = new Font("Courier New", Font.PLAIN, 16);
         textArea.setFont(courierNewFont);
         textArea.setText(fahrkartenText);
 		JOptionPane.showMessageDialog(druckerGUI,textArea);
