f72ee2f48c1275ef34b4b688e93b82956dda9075
akaibel <a.kaibel@googlemail.com>
2024-12-25 19:36:30 +0100

    Methoden angegeben, die programmiert werden sollen.

--- _test/StackTest.java
+++ _test/StackTest.java
@@ -21,9 +21,54 @@ public class StackTest {
 		//autoStack = Autos.autoStack();
 	}
 	
+	/**
+	 * die Namen von allen in celebritiesStack 
+	 * mit System.out.println(...) an die Konsole ausgeben
+	 * Damit der celebritiesStack nicht zerstoert wird, 
+	 * muss man jede Celebrity auf den Hilfsstack umschichten 
+	 * und am Ende wieder zurueck.
+	 * Zum Test mehrfach aufrufen!!
+	 */
 	public void ausgeben() {
+		Stack<Celebrity> hilfsStack = new StackWithViewer<>();
 		System.out.println("*** ausgeben ***");
+		
 		//TODO
+	}
+	
+	public int gesamtVermoegen() {
+		Stack<Celebrity> hilfsStack = new StackWithViewer<>();
+		//TODO
+		return -1;
+	}
+	
+	public boolean enthaelt(String pName) {
+		Stack<Celebrity> hilfsStack = new StackWithViewer<>();
+		//TODO
+		// VORSICHT: Vergleich von Strings mit equals!!
+		// z.B. if(pName.equals(...)){
+		return false;
+	}
+	
+	public Celebrity derReichste() {
+		Stack<Celebrity> hilfsStack = new StackWithViewer<>();
+		//TODO
+		return null;
+	}
+	
+	public void loeschen(String pName) {
+		Stack<Celebrity> hilfsStack = new StackWithViewer<>();
+		//TODO
+	}
+	
+	/**
+	 * um zu sortieren muss man die Methoden derReichste und loeschen geeignet aufrufen.
+	 * @return
+	 */
+	public Stack<Celebrity> sortierenNachVermoegen() {
+		Stack<Celebrity> ergebnis = new StackWithViewer<>();
+		//TODO
+		return ergebnis;
 	}
 
 	public static void main(String[] args) {
