e62d71247a765bc8f7c2c347c25e9fe14a323ae8
akaibel <a.kaibel@googlemail.com>
2025-07-16 17:37:02 +0200

    Platzhalter fuer DebuggingJamesBondAutomat

---
+++ _test/automaten/spionage/DebuggingJamesBondAutomat.java
@@ -0,0 +1,12 @@
+package _test.automaten.spionage;
+
+public class DebuggingJamesBondAutomat {
+	/**
+	 * Das hier ist nur ein Platzhalter!
+	 * 
+	 * Die richtige Klasse DebuggingJamesBondAutomat 
+	 * findet sich in folgendem package:
+	 * 
+	 * _test.debugging
+	 */
+}
