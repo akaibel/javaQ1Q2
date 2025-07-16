97b63647fd94a29cd614d35844ce68f4e1f3c70d
akaibel <a.kaibel@googlemail.com>
2025-07-16 17:34:21 +0200

    Änderungen beim James-Bond-Automat

--- _test/automaten/spionage/JamesBondAutomat.java
+++ _test/automaten/spionage/JamesBondAutomat.java
@@ -2,23 +2,35 @@ package _test.automaten.spionage;
 
 import gui.GUI;
 
-public class KGBAutomat {
+public class JamesBondAutomat {
 	
 	private char[] alphabet = {'0','1','2','3','4','5','6','7','8','9'};
 	
+	/**
+	 * prueft, ob pZeichen im alphabet enthalten ist.
+	 * @param pZeichen
+	 * @return
+	 */
 	public boolean imAlphabet(char pZeichen){
 		//TODO
 		return false;
 	}
 	
-	public boolean teste(String pEingabe){
+	/**
+	 * die Methode soll ueberpruefen, ob pEingabe die Zeichenkette "007" enthaelt.
+	 * Dafuer soll ein det. endl. Automat, der das prueft, in einen Parser uebersetzt werden.
+	 * 
+	 * @param pEingabe
+	 * @return
+	 */
+	public boolean enthaelt007(String pEingabe){
 		int zustand = 0;
 		//TODO
 		return false;
 	}
 	
 	public static void main(String[] args) {
-		new GUI(new KGBAutomat());
+		new GUI(new JamesBondAutomat());
 	}
 	
 }
--- _test/debugging/DebuggingJamesBondAutomat.java
+++ _test/debugging/DebuggingJamesBondAutomat.java
@@ -1,12 +1,24 @@
 package _test.debugging;
 
-public class DebuggingJamesBondAutomat {
+import javax.swing.*;
 
-	// das Testwort aendern!
-	private static final String TEST_WORT = "8";
-	// weitere Testwoerter: "007", "07", "3007"
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
 
-	public boolean enthaelt007(String pWort){
+public class DebuggingJamesBondAutomat extends JFrame {
+	/**
+	 * testWoerter bei Bedarf ergaenzen oder austauschen!
+	 */
+    private String[] testWoerter = {"8", "007", "07", "", "0700700", "3007"};
+
+    /**
+     * die folgende Methode funktioniert nicht richtig!
+     * Finde mithilfe von Debugging die Fehler und korrigiere sie.
+     * @param pWort
+     * @return
+     */
+    private boolean enthaelt007(String pWort) {
 	    int zustand = 0;
 	    for(int i=0; i<pWort.length(); i++){
 	        char zeichen = pWort.charAt(i);
@@ -18,9 +30,9 @@ public class DebuggingJamesBondAutomat {
 	            if(zeichen == 0) zustand = 2;
 	            else zustand = 0;
 	        }
-	        else if(zustand == 2){
+	        if(zustand == 2){
 	            if(zeichen == '0') zustand = 2;
-	            else if(zeichen == '7') zustand = 3;
+	            if(zeichen == '7') zustand = 3;
 	            else zustand = 0;
 	        }
 	        zustand = 3;
@@ -28,12 +40,70 @@ public class DebuggingJamesBondAutomat {
 	    //zurueckgeben, ob der Zustand 3 erreicht wurde.
 	    // die folgende Zeile ist fehlerfrei!
 	    return (zustand == 3);
-	 }
-	
-	public static void main(String[] args) {
-		DebuggingJamesBondAutomat jba = new DebuggingJamesBondAutomat();
-		boolean ergebnis = jba.enthaelt007(TEST_WORT);
-		System.out.println("Ergebnis fuer "+TEST_WORT);
-		System.out.println(ergebnis);
-	}
+    }
+
+    private JList<String> wortListe;
+    private JLabel ergebnisLabel;
+
+    public DebuggingJamesBondAutomat() {
+        setTitle("Test auf 007");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        // Schriftgröße 14
+        Font font = new Font("SansSerif", Font.PLAIN, 14);
+
+        // JList vorbereiten
+        wortListe = new JList<>(testWoerter);
+        wortListe.setFont(font);
+        wortListe.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        wortListe.setVisibleRowCount(testWoerter.length);
+
+        JScrollPane scrollPane = new JScrollPane(wortListe, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+        scrollPane.setMaximumSize(new Dimension(Integer.MAX_VALUE, wortListe.getPreferredSize().height));
+
+        // Ergebnis-Label
+        ergebnisLabel = new JLabel("Hier steht dann das Ergebnis.");
+        ergebnisLabel.setFont(font);
+        ergebnisLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        // Listener
+        wortListe.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                String ausgewaehlt = wortListe.getSelectedValue();
+                boolean enthaelt = enthaelt007(ausgewaehlt);
+                ergebnisLabel.setText("\"" + ausgewaehlt + "\" enthält 007? " + enthaelt);
+            }
+        });
+
+        // Panel für volle Breite
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        contentPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        JLabel hinweisLabel = new JLabel("Klicke ein Testwort an:");
+        hinweisLabel.setFont(font);
+        hinweisLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        contentPanel.add(hinweisLabel);
+        contentPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        contentPanel.add(scrollPane);
+        contentPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        contentPanel.add(ergebnisLabel);
+
+        setContentPane(contentPanel);
+        pack();
+
+        // Fensterbreite an Ergebnislabel anpassen
+        int labelWidth = ergebnisLabel.getPreferredSize().width + 100;
+        int currentWidth = getWidth();
+        int finalWidth = Math.max(currentWidth, labelWidth);
+        setSize(finalWidth, getHeight());
+
+        setLocationRelativeTo(null); // zentrieren
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new DebuggingJamesBondAutomat());
+    }
 }
